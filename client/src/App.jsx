06ecb712f6665828268980fe35a782a927d09829
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2024-07-23 18:41:27 +0300

    Add components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
+import GameCatalog from "./components/gameCatalog/GameCatalog";
+import GameCreate from "./components/gameCreate/GameCreate";
+import GameDetails from "./components/gameDetails/GameDetails";
+import GameEdit from "./components/gameEdit/GameEdit";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+
 function App() {
   return (
-    <>
-
-    </>
-  )
+    <div id="box">
+      <Header />
+      {/* <!-- Main Content --> */}
+      <main id="main-content">
+        <Home />
+        <Login />
+        <Register />
+        <GameCreate />
+        <GameEdit />
+        <GameDetails />
+        <GameCatalog />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
