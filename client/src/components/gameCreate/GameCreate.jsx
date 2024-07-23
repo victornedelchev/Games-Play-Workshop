b985bbd5fc68d797dc38326802b7f74f1f8ab942
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2024-07-23 22:21:38 +0300

    Add create game functionality

--- client/src/components/gameCreate/GameCreate.jsx
+++ client/src/components/gameCreate/GameCreate.jsx
@@ -1,8 +1,19 @@
+import { createGame } from "../../services/gameService";
+
 export default function GameCreate() {
+  const createGameSubmitHandler = async(e) => {
+    e.preventDefault();
+
+    const gameData = Object.fromEntries(new FormData(e.currentTarget));
+    const result = await createGame(gameData);
+
+    console.log(result);
+  };
+
   return (
     // <!-- Create Page ( Only for logged-in users ) -->
     <section id="create-page" className="auth">
-      <form id="create">
+      <form id="create" onSubmit={createGameSubmitHandler}>
         <div className="container">
           <h1>Create Game</h1>
           <label htmlFor="leg-title">Legendary title:</label>
