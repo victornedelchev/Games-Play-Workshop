9618a6758c9ca758d552293e7ff83f1bd6cf5c37
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2024-07-24 16:37:07 +0300

    Modify requiester

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,23 +1,14 @@
-import { request } from "../lib/request";
+import * as request from "../lib/request";
 
 const BASE_URL = "http://localhost:3030/jsonstore/games";
 
 export const getAllGames = async () => {
-  const result = await request("GET", BASE_URL);
+  const result = await request.get(BASE_URL);
 
   return Object.values(result);
 };
 
 export const createGame = async (gameData) => {
-  const response = await fetch(BASE_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(gameData),
-  });
-
-  const result = await response.json();
-
+  const result = await request.post(BASE_URL, gameData);
   return result;
 };
