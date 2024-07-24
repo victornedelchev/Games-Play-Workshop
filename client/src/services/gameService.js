5e3d6688e1d76b03d579bb4d93dc15af8a2cf255
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2024-07-24 15:43:46 +0300

    Add get all games servise service

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,7 +1,15 @@
-const BASE_URL = "http://localhost:3030/jsonstore";
+import { request } from "../lib/request";
+
+const BASE_URL = "http://localhost:3030/jsonstore/games";
+
+export const getAllGames = async () => {
+  const result = await request("GET", BASE_URL);
+
+  return Object.values(result);
+};
 
 export const createGame = async (gameData) => {
-  const response = await fetch(`${BASE_URL}/games`, {
+  const response = await fetch(BASE_URL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
