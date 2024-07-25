0a55e654cc9b43cc7db5927fb8fc4092234458b7
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2024-07-25 15:49:07 +0300

    Add alt attribute on image

--- client/src/components/gameDetails/GameDetails.jsx
+++ client/src/components/gameDetails/GameDetails.jsx
@@ -20,7 +20,7 @@ export default function GameDetails() {
       <h1>Game Details</h1>
       <div className="info-section">
         <div className="game-header">
-          <img className="game-img" src={game.imageUrl} />
+          <img className="game-img" src={game.imageUrl} alt={game.title} />
           <h1>{game.title}</h1>
           <span className="levels">MaxLevel: {game.maxLevel}</span>
           <p className="type">{game.category}</p>
