bb7d00764f3815c8a3e9342bebd8b518f92b2cca
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2024-08-08 19:52:56 +0300

    Add lazy loading for Details component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
+import { lazy, Suspense } from "react";
+
 import { Route, Routes } from "react-router-dom";
 
 import { AuthProvider } from "./contexts/authContext";
 
 import GameCatalog from "./components/gameCatalog/GameCatalog";
 import GameCreate from "./components/gameCreate/GameCreate";
-import GameDetails from "./components/gameDetails/GameDetails";
+// import GameDetails from "./components/gameDetails/GameDetails";
+const GameDetails = lazy(() => import("./components/gameDetails/GameDetails"));
 import GameEdit from "./components/gameEdit/GameEdit";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
@@ -25,11 +28,12 @@ function App() {
           {/* <!-- Main Content --> */}
           <Header />
           <main id="main-content">
-            <Routes>
-              <Route path={Path.Home} element={<Home />} />
-              <Route path={Path.Login} element={<Login />} />
-              <Route path={Path.Register} element={<Register />} />
-              {/* <Route
+            <Suspense fallback={<h1>Loading...</h1>}>
+              <Routes>
+                <Route path={Path.Home} element={<Home />} />
+                <Route path={Path.Login} element={<Login />} />
+                <Route path={Path.Register} element={<Register />} />
+                {/* <Route
                 path={Path.Create}
                 element={
                   <BaseAuthGuard>
@@ -37,15 +41,16 @@ function App() {
                   </BaseAuthGuard>
                   }
                   /> */}
-              <Route path={Path.Details} element={<GameDetails />} />
-              <Route path={Path.Catalog} element={<GameCatalog />} />
+                <Route path={Path.Details} element={<GameDetails />} />
+                <Route path={Path.Catalog} element={<GameCatalog />} />
 
-              <Route element={<AuthGuard />}>
-                <Route path={Path.Logout} element={<Logout />} />
-                <Route path={Path.Create} element={<GameCreate />} />
-                <Route path={Path.Edit} element={<GameEdit />} />
-              </Route>
-            </Routes>
+                <Route element={<AuthGuard />}>
+                  <Route path={Path.Logout} element={<Logout />} />
+                  <Route path={Path.Create} element={<GameCreate />} />
+                  <Route path={Path.Edit} element={<GameEdit />} />
+                </Route>
+              </Routes>
+            </Suspense>
           </main>
         </div>
       </AuthProvider>
